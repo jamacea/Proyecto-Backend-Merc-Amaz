c71d8b4905ced50dcf3e33a15d7d378ae78aaf58
Johan <jamacea@uninorte.edu.co>
2022-04-29 23:35:22 -0500

    Mas ordenado y user terminado

--- Backend/Servicios/Post.js
+++ Backend/Servicios/Post.js
@@ -1,93 +1,90 @@
 const mongoose = require("mongoose")
-const post_model= require('../Models/Model_post')
-const express=require("express")
+const post_model = require("../Models/Model_post")
+const express = require("express")
 const user_model = require("../Models/Model_user")
-const router=express.Router()
+const router = express.Router()
 
 //-----------------------------------Funciones y validaciones----------------------------------------------------
-const exist_owner=async(user_id)=>{
-    console.log(user_id)
-    const query=await user_model.findById(user_id)
-    console.log(query)
-    if(query!==null)
-    {return Object.keys(query).length!==0}else{return false}
+const exist_owner = async (user_id) => {
+	console.log(user_id)
+	const query = await user_model.findById(user_id)
+	console.log(query)
+	if (query !== null) {
+		return Object.keys(query).length !== 0
+	} else {
+		return false
+	}
 }
-
-
 
 //---------------------------------------------------------------------------------------------------------------
 
-router.post('/',async(req,res)=>{
-    const {user_id,img_url,display_name,description,price}=req.body
-    const user_exist= await exist_owner(user_id)
+router.post("/", async (req, res) => {
+	const {user_id, img_url, display_name, description, price} = req.body
+	const user_exist = await exist_owner(user_id)
 
-   if( user_exist && img_url && display_name && description && price){
-        try{const doc= new post_model({user_id,img_url,display_name,description,price})
-        await doc.save()
-        res.status(201).json(doc)        
-    }catch(e){
-        console.log(e)
-        res.status(500).json({"message":"not able to create"})
-
-    }
-    }else{
-        res.status(500).json({"message":"error, documento no se pudo crear"})
-    }
-
-
+	if (user_exist && img_url && display_name && description && price) {
+		try {
+			const doc = new post_model({
+				user_id,
+				img_url,
+				display_name,
+				description,
+				price,
+			})
+			await doc.save()
+			res.status(201).json(doc)
+		} catch (e) {
+			console.log(e)
+			res.status(500).json({message: "not able to create"})
+		}
+	} else {
+		res.status(500).json({message: "error, documento no se pudo crear"})
+	}
 })
 
-
-router.get('/recent',async(req,res,next)=>{
-    try{
-        const query= await post_model.find()
-        res.status(200).json(query)
-
-    }catch(e){
-        console.log(e)
-        res.status(500).json(e)
-    }
+router.get("/recent", async (req, res, next) => {
+	try {
+		const query = await post_model.find()
+		res.status(200).json(query)
+	} catch (e) {
+		console.log(e)
+		res.status(500).json(e)
+	}
 })
 
+router.get("/", async (req, res) => {
+	const {post_id} = req.query
+	const {user_id} = req.query
+	if (post_id) {
+		try {
+			const query = await post_model.findById(post_id)
 
-router.get('/',async(req,res)=>{
-    const {post_id}=req.query
-    const {user_id}=req.query
-    if(post_id){
-
-        try{
-            const query= await post_model.findById(post_id)
-
-            res.status(200).json(query)
-
-        }catch(e){
-            console.log(e)
-            res.status(404).json({"message":"Not found remember to take out \"\" from the id"})
-        }
-
-    }else if(user_id){
-        const existe=await exist_owner(user_id)
-        if(existe){
-            
-            try{
-            const query= await post_model.find({user_id:mongoose.Types.ObjectId(user_id)})
-            res.status(200).json(query)}
-            catch(e){
-                console.log(e)
-                res.status(500).json({"message":"error"})
-            }
-
-        }else{
-            res.status(404).json({"message":"user doesn't exists"})
-        }
-
-    }else{
-        res.status(500).json({"message":"invalid query"})
-    }
-
-
+			res.status(200).json(query)
+		} catch (e) {
+			console.log(e)
+			res
+				.status(404)
+				.json({message: 'Not found remember to take out "" from the id'})
+		}
+	} else if (user_id) {
+		const existe = await exist_owner(user_id)
+		if (existe) {
+			try {
+				const query = await post_model.find({
+					user_id: mongoose.Types.ObjectId(user_id),
+				})
+				res.status(200).json(query)
+			} catch (e) {
+				console.log(e)
+				res.status(500).json({message: "error"})
+			}
+		} else {
+			res.status(404).json({message: "user doesn't exists"})
+		}
+	} else {
+		res.status(500).json({message: "invalid query"})
+	}
 })
-
 
 // router.get('/',async(req,res)=>{
 //     const {user_id}=req.query
@@ -95,7 +92,7 @@ router.get('/',async(req,res)=>{
 //     if(user_id){
 //         const existe=await exist_owner(user_id)
 //         if(existe){
-            
+
 //             try{
 //             const query= await post_model.find({user_id:mongoose.Types.ObjectId(user_id)})
 //             res.status(200).json(query)}
@@ -108,12 +105,10 @@ router.get('/',async(req,res)=>{
 //             res.status(404).json({"message":"user doesn't exists"})
 //         }
 
-
 //     }else{
 //         res.status(404).json({"message":"not a good user id"})
 //     }
 // })
 
 //---------------------------------------------------------------------------------------------------------------
-module.exports=router
-
+module.exports = router
--- Backend/Servicios/User.js
+++ Backend/Servicios/User.js
@@ -1,42 +1,41 @@
 const mongoose = require("mongoose")
-const user_model= require('../Models/Model_user')
-const express=require("express")
-const router=express.Router()
-
+const user_model = require("../Models/Model_user")
+const express = require("express")
+const router = express.Router()
 
 // display_name: String,
 // username: String,
 // password: String
 //-----------------------------------Funciones y validaciones----------------------------------------------------
 
-
-const create=(display_name="",username="",password="")=>{
-    return {
-        display_name: display_name,
-        username:username,
-        password:password}
-    }
-
-
-const valido=(sesion)=>{
-    const {username,password}=sesion
-    return Boolean(username && password)
+const create = (display_name = "", username = "", password = "") => {
+	return {
+		display_name: display_name,
+		username: username,
+		password: password,
+	}
 }
 
-const validarusuario= async (username)=>{    
-    const query=await user_model.findOne({username})
-    if(query){
-    return  Object.keys(query).length==0?true:false}else{
-        return true
-    }
+const valido = (sesion) => {
+	const {username, password} = sesion
+	return Boolean(username && password)
 }
 
-const id_exist= async(user_id)=>{
-    const query= await user_model.findOne({user_id:mongoose.Types.ObjectId(user_id)})
-    return Boolean(query)
+const validarusuario = async (username) => {
+	const query = await user_model.findOne({username})
+	if (query) {
+		return Object.keys(query).length == 0 ? true : false
+	} else {
+		return true
+	}
 }
 
-
+const id_exist = async (user_id) => {
+	const query = await user_model.findOne({
+		user_id: mongoose.Types.ObjectId(user_id),
+	})
+	return Boolean(query)
+}
 
 // const build_id= async (_id)=>{
 //     const query=await user_model.()
@@ -47,110 +46,111 @@ const id_exist= async(user_id)=>{
 // let resultado={a}
 // console.log(resultado)
 
-
-
 // console.log(Boolean("username" && "a")) //true
 // console.log(Boolean("username" && "b")) //false
 
 //-----------------------------------------------------------------------------------------------
 
-
-
-
-router.get('/all',async (req,res)=>{
-    try{
-        const users=await user_model.find()
-        res.send(users)
-    }catch(e){
-        console.log(e)
-    }
-
+router.get("/all", async (req, res) => {
+	try {
+		const users = await user_model.find()
+		res.send(users)
+	} catch (e) {
+		console.log(e)
+	}
 })
 
-router.post('/register',async (req,res)=>{
-    try{
-        const {owner_id,display_name,username,password}=req.body
-        console.log(username)
-        allowed= await validarusuario(username)
-        if(allowed){
-        const registrar=create(display_name,username,password)
-        const newUser= new user_model(registrar)
-        // const{_id}=newUser
-        // newUser.user_id=_id
-        await newUser.save()
-        res.status(200).json({"message":"Succesfully created"})
-    }else{
-            res.status(400).json({"message":"Already exist, try another username"})
-        }
-    }catch(e){
-        console.log(e)
-    }
-
+router.post("/register", async (req, res) => {
+	try {
+		const {owner_id, display_name, username, password} = req.body
+		console.log(username)
+		allowed = await validarusuario(username)
+		if (allowed) {
+			const registrar = create(display_name, username, password)
+			const newUser = new user_model(registrar)
+			// const{_id}=newUser
+			// newUser.user_id=_id
+			await newUser.save()
+			res.status(200).json({message: "Succesfully created"})
+		} else {
+			res.status(400).json({message: "Already exist, try another username"})
+		}
+	} catch (e) {
+		console.log(e)
+	}
 })
 
-router.post('/login',async (req,res)=>{
-    let {username,password}=req.body
-    let sesion={username,password}
-    if(valido(sesion)){
-      try{  
-        const query= await user_model.findOne({username,password})
-        res.status(200).json(query)
-    }catch(e){
-        console.log(e)
-    res.status(400).json({"message":e})
-    }
-    }else{
-        res.status(404).json({"message":"not found"})
-    }
+router.post("/login", async (req, res) => {
+	let {username, password} = req.body
+	let sesion = {username, password}
+	if (valido(sesion)) {
+		try {
+			const query = await user_model.findOne({username, password})
+			res.status(200).json(query)
+		} catch (e) {
+			console.log(e)
+			res.status(400).json({message: e})
+		}
+	} else {
+		res.status(404).json({message: "not found"})
+	}
 })
 
-router.post('/',async(req,res)=>{
-    try{
-        const {user_id}=req.body
-        console.log(user_id)
-        const query=await user_model.findById(user_id).exec()
-        if(Object.keys(query)){
-            res.status(200).json(query)
-        }else{ 
-            res.status(404).json({"message":"Doesn't exist that id"})
-        }
-    }catch(e){
-        console.log('error-----'+e)
-        res.status(400).json({"message":"Doesn't exist that id"})
-    }
+router.post("/", async (req, res) => {
+	try {
+		const {user_id} = req.body
+		console.log(user_id)
+		const query = await user_model.findById(user_id).exec()
+		if (Object.keys(query)) {
+			res.status(200).json(query)
+		} else {
+			res.status(404).json({message: "Doesn't exist that id"})
+		}
+	} catch (e) {
+		console.log("error-----" + e)
+		res.status(400).json({message: "Doesn't exist that id"})
+	}
 })
 
-
-// router.post('/prev-login',async (req,res)=>{
-
-
-// })
-
-
+router.get("/prev-login", async (req, res) => {
+	const {user_id} = req.query
+	if (user_id) {
+		const exist = await id_exist(user_id)
+		if (exist) {
+			try {
+				const query = await user_model.findById(user_id)
+				res.status(200).json(query)
+			} catch (e) {
+				res.status(404).json({message: "not found"})
+			}
+		}
+	} else {
+		res.status(404).json({message: "not found"})
+	}
+})
 
 // Funciona
-router.post('/update',async(req,res)=>{
-    const {user_id,lastname}=req.body
-    if(user_id && lastname){
-    if(id_exist(user_id)){    
-     try{   
-     const pipeline=[{$set:{"lastname":lastname}}]    
-     const query=await user_model.updateOne({user_id:mongoose.Types.ObjectId(user_id)},pipeline)
-     res.status(200).json(query)
-    }catch(e){
-        console.log(e)
-        res.status(404).json({message:"Bad request"})
+// router.post("/update", async (req, res) => {
+// 	const {user_id, lastname} = req.body
+// 	if (user_id && lastname) {
+// 		if (id_exist(user_id)) {
+// 			try {
+// 				const pipeline = [{$set: {lastname: lastname}}]
+// 				const query = await user_model.updateOne(
+// 					{user_id: mongoose.Types.ObjectId(user_id)},
+// 					pipeline
+// 				)
+// 				res.status(200).json(query)
+// 			} catch (e) {
+// 				console.log(e)
+// 				res.status(404).json({message: "Bad request"})
+// 			}
+// 		} else {
+// 			res.status(404).json({message: "user_id doesn't exist"})
+// 		}
+// 	} else {
+// 		res.status(404).json({message: "Bad request"})
+// 	}
+// })
 
-     }
-    }else{
-        res.status(404).json({message:"user_id doesn't exist"})
-    }
-    }else{
-        res.status(404).json({message:"Bad request"})
-    }
-
-
-})
-
-module.exports=router
-
+module.exports = router
